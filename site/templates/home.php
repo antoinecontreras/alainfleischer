a4f7a5b1e16cb8d912ee37e1bbb8a59aaa0b8b0a
antoinecontreras <antoine.ctrs@gmail.com>
2024-11-05 15:55:55 +0100

    test

--- site/templates/home.php
+++ site/templates/home.php
@@ -32,88 +32,92 @@ foreach ($projects as $project) {
 							<img class="" draggable="false" src="<?= $project->img->url ?>" />
 						</div>
 					<?php endif; ?>
-					<h2 class="nav_cartel"><?= $project->textarea; ?></h2>
-					<p><?= $project->date . ' - ' . $project->date_end; ?></p>
-					<?php if ($project->gallery) : ?>
-						<div class="nav_gallery">
+					<div>
+						<h2 class="nav_cartel"><?= $project->textarea; ?></h2>
+						<p><?= $project->date . ' - ' . $project->date_end; ?></p>
+					</div>
+						<?php if ($project->gallery) : ?>
+							<div class="nav_gallery">
 							<?php
 
 							foreach ($project->gallery as $image) : ?>
-								<img draggable="false" width="300" src="<?php echo $image->url; ?>" />
+								<div class="nav_image">
+									<img draggable="false" width="300" src="<?php echo $image->url; ?>" />
+								</div>
 							<?php endforeach; ?>
 
 						<?php endif; ?>
 						</div>
-				</div>
-			<?php endforeach; ?>
-		<?php endforeach; ?>
-	</div>
-
-
-
-
-	<div class="page_row collection_tab">
-		<div class="collection_filter">
-			<?php
-			$counter = 0;
-
-			foreach ($projectsByCategory as $categoryTitle => $projects) :
-				$filters = [];
-				foreach ($projects as $project) :
-					$filterTitle = $project->filter->title;
-
-					if (!isset($filters[$filterTitle])) :
-						$filters[$filterTitle] = $filterTitle; ?>
-						<a draggable="false" href="#<?= urlencode($filterTitle); ?>" class="filter-link" onclick="highlightLink('<?= $filterTitle; ?>')"><?= $filterTitle ?></a>
-			<?php endif;
-				endforeach;
-			endforeach; ?>
-		</div>
-
-		<!-- Section des projets -->
-		<?php foreach ($projectsByCategory as $categoryTitle => $projects) : ?>
-			<div id="<?= urlencode($categoryTitle); ?>" class="collection_category">
-				<?php
-				$projectsByYear = [];
-				foreach ($projects as $project) :
-					$year = date('Y', strtotime($project->date));
-					$projectsByYear[$year][] = $project;
-				endforeach;
-				foreach ($projectsByYear as $year => $projects) :
-					echo "<p class='collection_year'>{$year}</p>"; ?>
-					<div class='collection_items'>
-						<?php foreach ($projects as $project) :
-
-							$filterRef =  urlencode($project->filter->title);
-							$projectRef =  urlencode($project->title);
-							$parentRef =  urlencode($project->parent->title);
-						?>
-							<a
-								data-filter="<?= $filterRef ?>"
-								data-project="<?= $projectRef ?>"
-								data-parent="<?= $parentRef ?>"
-								id="<?= urlencode($project->title); ?>" draggable="false" class="collection_item interactive_item <?= $project->img ? 'with_img' : ''; ?>">
-								<?php if ($project->img) :
-									$img = $project->img;
-									$focusX = $img->focus["left"];
-									$focusY = $img->focus["top"];
-								?>
-									<img draggable="false" src="<?= $img->url; ?>"
-										alt="<?= $project->title; ?>"
-										style=" 
-									transform-origin: <?= $focusX; ?>% <?= $focusY; ?>%;" />
-								<?php endif; ?>
-							</a>
-
-
-
-						<?php endforeach; ?>
 					</div>
-				<?php endforeach;
-				?>
-			</div> <!-- Fin de la catégorie -->
-		<?php endforeach; ?>
+				<?php endforeach; ?>
+			<?php endforeach; ?>
+				</div>
+
+
+
+
+				<div class="page_row collection_tab">
+					<div class="collection_filter">
+						<?php
+						$counter = 0;
+
+						foreach ($projectsByCategory as $categoryTitle => $projects) :
+							$filters = [];
+							foreach ($projects as $project) :
+								$filterTitle = $project->filter->title;
+
+								if (!isset($filters[$filterTitle])) :
+									$filters[$filterTitle] = $filterTitle; ?>
+									<a draggable="false" href="#<?= urlencode($filterTitle); ?>" class="filter-link" onclick="highlightLink('<?= $filterTitle; ?>')"><?= $filterTitle ?></a>
+						<?php endif;
+							endforeach;
+						endforeach; ?>
+					</div>
+
+					<!-- Section des projets -->
+					<?php foreach ($projectsByCategory as $categoryTitle => $projects) : ?>
+						<div id="<?= urlencode($categoryTitle); ?>" class="collection_category">
+							<?php
+							$projectsByYear = [];
+							foreach ($projects as $project) :
+								$year = date('Y', strtotime($project->date));
+								$projectsByYear[$year][] = $project;
+							endforeach;
+							foreach ($projectsByYear as $year => $projects) :
+								echo "<p class='collection_year'>{$year}</p>"; ?>
+								<div class='collection_items'>
+									<?php foreach ($projects as $project) :
+
+										$filterRef =  urlencode($project->filter->title);
+										$projectRef =  urlencode($project->title);
+										$parentRef =  urlencode($project->parent->title);
+									?>
+										<a
+											data-filter="<?= $filterRef ?>"
+											data-project="<?= $projectRef ?>"
+											data-parent="<?= $parentRef ?>"
+											id="<?= urlencode($project->title); ?>" draggable="false" class="collection_item interactive_item <?= $project->img ? 'with_img' : ''; ?>">
+											<?php if ($project->img) :
+												$img = $project->img;
+												$focusX = $img->focus["left"];
+												$focusY = $img->focus["top"];
+											?>
+												<img draggable="false" src="<?= $img->url; ?>"
+													alt="<?= $project->title; ?>"
+													style=" 
+									transform-origin: <?= $focusX; ?>% <?= $focusY; ?>%;" />
+											<?php endif; ?>
+										</a>
+
+
+
+									<?php endforeach; ?>
+								</div>
+							<?php endforeach;
+							?>
+						</div> <!-- Fin de la catégorie -->
+					<?php endforeach; ?>
+				</div>
+
+
 	</div>
-
-
-</div>
